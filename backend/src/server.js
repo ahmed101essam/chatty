9528bea8756b347b2d6d28359b48a88f63d58f81
ahmed1shehab <ahmed10pepeto@gmail.com>
2026-03-27 01:41:07 +0200

    ready to development

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 // const express = require("express");
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
@@ -9,9 +11,17 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "../frontend")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
